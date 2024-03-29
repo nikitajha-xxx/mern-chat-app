8417d3b89d8559a684bcf96f2d155c5c6bd04edb
nikitajha-xxx <jhanikita85@gmail.com>
2024-03-29 23:09:38 +0530

    adding fetch, rename, create group, add user and remove user from group routes in backend

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -50,4 +50,143 @@ const accessChat = asyncHandler(async(req,res)=>{
 
 })
 
-module.exports = {accessChat}
+//@description     Fetch all chats for a user
+//@route           GET /api/chat/
+//@access          Protected
+const fetchChats = asyncHandler(async(req,res)=>{
+    try{
+        Chat.find({users:{$elemMatch:{$eq:req.user._id}}})
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password")
+            .populate("latestMessage")
+            .sort({updatedAt:-1}) //sort the output from new to old
+            .then(async(results)=>{
+                results = await  User.populate(results,{
+                    path:"latestMessage.sender", 
+                    select:"name pic email"
+                })
+                res.status(200).send(results);
+            })
+    }catch(err){
+        res.status(400);
+        throw new Error(err.message);
+    }
+})
+
+//@description     Create New Group Chat
+//@route           POST /api/chat/group
+//@access          Protected
+const createGroupChat = asyncHandler(async(req,res)=>{
+    if(!req.body.users || !req.body.name){
+        return res.status(400).send({message:"Please fill in all the fields"})
+    }
+    var users = JSON.parse(req.body.users)
+
+    if(users.length < 2){
+        return res
+            .status(400)
+            .send("More than 2 users are required to form a group chat")
+    }
+
+    users.push(req.user)
+
+    try{
+        const groupChat = await Chat.create({
+            chatName:req.body.name,
+            users:users,
+            isGroupChat:true,
+            groupAdmin:req.user
+        })
+        const fullGroupChat = await Chat.findOne({_id:groupChat._id})
+            .populate("users", "-password")
+            .populate("groupAdmin", "-password")
+        
+        return res.status(200).json(fullGroupChat)
+    }catch(err){
+        res.status(400)
+        throw new Error(err.message)
+
+    }
+})
+
+// @desc    Rename Group
+// @route   PUT /api/chat/rename
+// @access  Protected
+const renameGroup = asyncHandler(async(req,res)=>{
+    const {chatId, chatName} = req.body
+    
+    const updatedChat = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            chatName:chatName
+        },{
+            new:true //this will return the updated value of the chat name
+        }
+    )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+
+    if(!updatedChat){
+        res.status(404);
+        throw new Error("Chat Not Found")
+    }else{
+        res.json(updatedChat)
+    }
+})
+
+// @desc    Add user to Group / Leave
+// @route   PUT /api/chat/groupadd
+// @access  Protected
+const addToGroup = asyncHandler(async(req,res)=>{
+    const {chatId, userId} = req.body
+    const added = await Chat.findByIdAndUpdate(
+        chatId,
+        {
+            $push: {users:userId} //The $push operator appends a specified value to an array.
+        },
+        {new:true}
+    )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+
+    if(!added){
+        res.status(400)
+        throw new Error("Chat Not Found")
+    }else{
+        res.json(added)
+    }
+})
+
+// @desc    Remove user from Group
+// @route   PUT /api/chat/groupremove
+// @access  Protecte
+const removeFromGroup = asyncHandler(async(req,res)=>{
+    const {chatId, userId} = req.body
+
+    //check if the requester is the admin
+    var chat = await Chat.findById({_id:chatId})
+    // chat.populate("groupAdmin", )
+    if(req.user._id != chat.groupAdmin){
+        return res
+        .status(400)
+        .send(`${req.user._id}, ${chat.groupAdmin}`)
+    }
+
+    const removed = chat.update(
+        {
+            $pull: {users:userId} //The $pull operator removes from an existing array all instances of a value or values that match a specified condition.
+        },
+        {new:true}
+    )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password")
+
+    if(!removed){
+        res.status(400)
+        throw new Error("Chat Not Found")
+    }else{
+        res.json(removed)
+    }
+})
+
+module.exports = {accessChat, fetchChats,createGroupChat,renameGroup, addToGroup, removeFromGroup}
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,22 +1,25 @@
 var express = require('express')
 var router = express.Router()
 const {protect} = require('../middleware/authMiddleware')
-var {accessChat} = require('../controllers/chatControllers')
+var {accessChat, fetchChats,createGroupChat, renameGroup, addToGroup,removeFromGroup} = require('../controllers/chatControllers')
 
 //ROUTE TO Create or fetch One to One Chat
 router.route('/').post(protect,accessChat)
-// .get(protect,fetchChats) //can chain multiple type of requests. //any backend call that comes to fetchChats will first have to pass the protect middleware
+
+
+//ROUTE TO FETCH ALL CHATS FOR THE CURRENT LOGGED IN USER
+router.route('/').get(protect,fetchChats) //can chain multiple type of requests. //any backend call that comes to fetchChats will first have to pass the protect middleware
 
 //ROUTE FOR CREATION OF A GROUP
-// router.route('/group').post(protect,createGroupChat);
+router.route('/group').post(protect,createGroupChat);
 
-// //ROUTE FOR RENAMING A GROUP
-// router.route('/rename').put(protect, renameGroup);
+// ROUTE FOR RENAMING A GROUP
+router.route('/rename').put(protect, renameGroup);
 
-// //ROUTE FOR REMOVING A USER FROM A GROUP OR LEAVING A GROUP
-// router.route('/groupremove').put(protect,removeFromGroup);
+//ROUTE FOR REMOVING A USER FROM A GROUP OR LEAVING A GROUP
+router.route('/groupremove').put(protect,removeFromGroup);
 
-// //ROUTE FOR ADDING A NEW USER IN A GROUP
-// router.route('/groupadd').put(protect,addToGroup);
+// ROUTE FOR ADDING A NEW USER IN A GROUP
+router.route('/groupadd').put(protect,addToGroup);
 
 module.exports = router
